361b69a4009eb97917550bd1568a846a2f23cb9a
Sejin <sejinbag42@gmail.com>
2025-03-07 20:42:56 +0900

    bootstrap 추가

--- server/app.js
+++ server/app.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var cors = require('cors')
-app.use(cors());
+// var cors = require('cors')
+// app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
